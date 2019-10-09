5dd4c5f773e05da553947d1456346fafc1f67be9
Daniel Lee <leedc@me.com>
2019-10-09 19:13:57 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,8 +16,7 @@ class String
 
   def count_sentences
     #count string as 1 if end_with punctuation
-    sentences = self.split(/\?|\.|!/).reject
-    sentences.count
+    sentences = self.split(/[?.!\s/]/).count
   end
   binding.pry
 end
