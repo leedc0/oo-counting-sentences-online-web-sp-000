016afbcb975f5c59effca2e67733c1ce20d3e495
Daniel Lee <leedc@me.com>
2019-10-09 18:28:41 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,18 +4,10 @@ class String
 
   def sentence?
     self.end_with?(".") ? true : false
-    # if self.end_with?(".")
-    #   return true
-    # else
-    #   return false
-    # end
   end
 
   def question?
-    self.end_with?(".") ? true : false
-    # if self.end_with?(".")
-    #   return true
-    # end
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
